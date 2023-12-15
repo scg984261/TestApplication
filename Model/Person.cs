c3cab5625a67a97311667fa1e74fc1d504a5aeea
scottgillen <scottg159@gmail.com>
2023-12-15 18:28:43 +0000

    Removed bin files from git repo

--- Model/Person.cs
+++ Model/Person.cs
@@ -1,10 +1,10 @@
 namespace Model
 {
-    public class Person
+    public class Person : IPrintable
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-
+        
         public void PrintDetails()
         {
             Console.WriteLine($"ID: {Id}");
